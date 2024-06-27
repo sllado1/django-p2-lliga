1e49d5b05af7afa21ab9d1ce0771eeda25375564
sllado1 <sllado1@xtec.cat>
2024-06-27 16:18:07 +0200

    View v0

--- lliga/views.py
+++ lliga/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from .models import Lliga
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def classificacio(request):
+    lliga = Lliga.objects.first()
+    equips = lliga.equips.all()
+    classi = []
+ 
+    # calculem punts en llista de tuples (equip,punts)
+    for equip in equips:
+        punts = 0
+        for partit in lliga.partit_set.filter(local=equip):
+            if partit.gols_local() > partit.gols_visitant():
+                punts += 3
+            elif partit.gols_local() == partit.gols_visitant():
+                punts += 1
+        for partit in lliga.partit_set.filter(visitant=equip):
+            if partit.gols_local() < partit.gols_visitant():
+                punts += 3
+            elif partit.gols_local() == partit.gols_visitant():
+                punts += 1
+        classi.append( (punts,equip.nom) )
+    # ordenem llista
+    classi.sort(reverse=True)
+    return render(request,"classificacio.html",
+                {
+                    "classificacio":classi,
+                })
